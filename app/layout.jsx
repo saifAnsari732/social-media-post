814b0a1e0e97bdbf4c6071a96d7790d216ce4e80
saif ansari <kisandeveloper2@gmail.com>
2026-08-02 15:09:00 +0530

    Add Google site verification tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,10 @@ import "./globals.css";
 
 export const metadata = {
   title: "yt-post — One Video, Every Channel",
-  description: "yt-post is an application that allows users to securely connect their social media accounts and publish video content directly to their YouTube channels and other platforms."
+  description: "yt-post is an application that allows users to securely connect their social media accounts and publish video content directly to their YouTube channels and other platforms.",
+  verification: {
+    google: "PGNbKPaL5LatP1QDJc73lVa9CPnWeAO5AebFE1xIsm0",
+  },
 };
 
 export default function RootLayout({ children }) {
