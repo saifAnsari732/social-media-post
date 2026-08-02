0c586e494421b43e9580485120dfc58d3784cbe8
saif ansari <kisandeveloper2@gmail.com>
2026-08-02 17:10:20 +0530

    Remove offline.access scope from Twitter OAuth to test Free tier restrictions

--- app/api/auth/connect/[provider]/route.js
+++ app/api/auth/connect/[provider]/route.js
@@ -16,7 +16,7 @@ const AUTH_URLS = {
     )}`,
   twitter: () =>
     `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${process.env.TWITTER_CLIENT_ID}&redirect_uri=${process.env.TWITTER_REDIRECT_URI}&scope=${encodeURIComponent(
-      "tweet.read tweet.write users.read offline.access"
+      "tweet.read tweet.write users.read"
     )}&state=state&code_challenge=yDiyogOBAhICYCLz2kezunB7Mo0MdVRIQMs7tRbJmLE&code_challenge_method=S256`,
   linkedin: () =>
     `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENT_ID}&redirect_uri=${process.env.LINKEDIN_REDIRECT_URI}&scope=${encodeURIComponent(
