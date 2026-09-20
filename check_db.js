75e6f31cc7407e53c217ddd1a51d14b589424ab0
saif ansari <kisandeveloper2@gmail.com>
2026-09-20 15:37:02 +0530

    chore: remove accidental temp files

--- check_db.js
+++
@@ -1,9 +0,0 @@
-import clientPromise from './lib/mongodb.js';
-async function run() {
-  const client = await clientPromise;
-  const db = client.db();
-  const settings = await db.collection('settings').find({}).toArray();
-  console.log('Settings:', settings);
-  process.exit(0);
-}
-run();
